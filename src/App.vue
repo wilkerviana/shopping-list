09bf1110083dee10fa3f8dea4976272a4a5a0352
wilkerviana <wilker.info@gmail.com>
2017-11-29 01:46:03 -0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        title: 'Shopping List'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700');
+  // Colors
+  color-main = #5ecf43
+  // Fonts
+  font = 'Source Sans Pro', sans-serif;
+
+  // Reset
+  *
+    box-sizing: border-box
+    margin: 0
+    padding: 0
+
+  body
+    background: color-main
+    color: #fff
+    font-family: font
+</style>
+
